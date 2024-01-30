5ba48bfaf5ed7b026f53f6f489477145e1ad1005
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-30 03:20:33 +0300

    100DataAPI

--- json.js
+++ json.js
@@ -1,28 +1,36 @@
 console.log("json.js")
-
-
-
-
 const allData = () => {
 
-    fetch('https://randomuser.me/api/?results=50')
+    fetch('https://randomuser.me/api/?results=100 ')
     .then(res => res.json())
     .then(findOut => userInfo(findOut.results))
-
-
-
-}
+    }
 allData()
+
+    const userDataSection = document.querySelector(".userDataSection");
+    console.log(userDataSection)
+
 const userInfo = (datas) => {
-
-        for(const data of datas){
-
-
-            console.log(data)
-        }
-
-
-
-
-
+    
+    for(const data of datas){
+        const div = document.createElement("div");
+            div.style.background = "gray"
+            div.style.padding = "15px"
+            div.style.margin = "20px"
+            div.style.borderRadius = "10px"
+            div.innerHTML =`
+                <P>User Id : ${data.id.value} </P>
+                <p>Title Name : ${data.name.title}</p>
+                <p>Fist name : ${data.name.first} </p>
+                <p>Last name : ${data.name.last}</p>
+                <p>Phone : ${data.cell}</p>
+                <p>E-mail : ${data.email}</p>
+                <p>DOB : ${data.dob.date} </p>
+                <p>Age : ${data.dob.age}</p>
+                <p>Gender : ${data.gender}</p>
+                <p>Location : ${data.location.country} </p>
+            `
+        userDataSection.appendChild(div)
+        // console.log(div)
+    }
 }
